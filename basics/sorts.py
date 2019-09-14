c15b0571e59bf70d06c62401f9f892c819dd894a
Caner Derici <canerderici@gmail.com>
2019-09-13 20:05:21 -0400

    msort

--- basics/sorts.py
+++ basics/sorts.py
@@ -1,5 +1,6 @@
 # quick sort
 
+# in-place
 def q_sort(ls):
     return q_sort_iter(ls, 0, len(ls)-1)
 
@@ -40,6 +41,7 @@ assert q_sort([4,2,3,8,6,5,1,7]) == [1,2,3,4,5,6,7,8]
 assert q_sort([2,6,1,7,4,9,5]) == [1,2,4,5,6,7,9]
 assert q_sort([6,1,7,4]) == [1,4,6,7]
 
+# if we're ok with using a lot of space
 def q_sort_rec(ls):
     if len(ls) <= 1:
         return ls
@@ -58,3 +60,44 @@ assert q_sort_rec([4,2,3,6,5,1,7]) == [1,2,3,4,5,6,7]
 assert q_sort_rec([4,2,3,8,6,5,1,7]) == [1,2,3,4,5,6,7,8]
 assert q_sort_rec([2,6,1,7,4,9,5]) == [1,2,4,5,6,7,9]
 assert q_sort_rec([6,1,7,4]) == [1,4,6,7]
+
+
+# merge sort
+
+# O(nlogn) space
+# could be made to use O(n) space
+def m_sort(ls):
+    if len(ls) <= 1:
+        return ls
+    mid_idx = len(ls)/2
+    return merge(m_sort(ls[:mid_idx]), m_sort(ls[mid_idx:]))
+
+def merge(l1,l2):
+    ret = []
+    # like sorted card decks
+    while l1 != [] and l2 != []:
+        if l1[0] < l2[0]:
+            ret.append(l1.pop(0))
+        else:
+            ret.append(l2.pop(0))
+
+    # either l1 or l2 might still be non-empty
+    if l1 != []:
+        ret.extend(l1)
+    if l2 != []:
+        ret.extend(l2)
+
+    return ret
+
+assert m_sort([]) == []
+assert m_sort([1]) == [1]
+assert m_sort([2,1]) == [1,2]
+assert m_sort([2,3,1]) == [1,2,3]
+assert m_sort([4,2,3,1]) == [1,2,3,4]
+assert m_sort([4,2,2,3,1]) == [1,2,2,3,4]
+assert m_sort([4,2,3,5,1]) == [1,2,3,4,5]
+assert m_sort([4,2,3,6,5,1]) == [1,2,3,4,5,6]
+assert m_sort([4,2,3,6,5,1,7]) == [1,2,3,4,5,6,7]
+assert m_sort([4,2,3,8,6,5,1,7]) == [1,2,3,4,5,6,7,8]
+assert m_sort([2,6,1,7,4,9,5]) == [1,2,4,5,6,7,9]
+assert m_sort([6,1,7,4]) == [1,4,6,7]
