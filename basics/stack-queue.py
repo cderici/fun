f00e17413a715de5eb660a284bb05df9fdcf65c9
Caner Derici <canerderici@gmail.com>
2019-09-29 10:59:59 -0400

    queue with double stack

--- basics/stack-queue.py
+++ basics/stack-queue.py
@@ -106,3 +106,72 @@ q.enqueue(1)
 assert q.dequeue() == 3
 assert q.dequeue() == 2
 assert q.dequeue() == 1
+
+
+class Stack(object):
+    def __init__(self):
+        self.container = []
+
+    def push(self, data):
+        self.container.append(data)
+
+    def pop(self):
+        return self.container.pop()
+
+    def is_empty(self):
+        return self.container == []
+
+class Queue(object):
+    def __init__(self):
+        self.stack1 = Stack()
+        self.stack2 = Stack()
+
+    def enqueue(self, data):
+        # enqueue into stack1
+        # check stack full if there's a stack size limit
+        self.stack1.push(data)
+
+    def is_empty(self):
+        return self.stack1.is_empty() and self.stack2.is_empty()
+
+    def dequeue(self):
+        # dequeue from stack2
+        if self.is_empty():
+            raise Exception("nothing to dequeue")
+
+        if self.stack2.is_empty():
+            self.transfer()
+        return self.stack2.pop()
+
+    def transfer(self):
+        # transfer everything from stack1 to stack2
+        while not self.stack1.is_empty():
+            self.stack2.push(self.stack1.pop())
+
+""" assumes the stacks don't have limits
+  s1
+|    |
+|    |
+|    |
+|____|
+
+  s2
+|    |
+|    |
+|    |
+|_2__|
+
+
+"""
+
+q = Queue()
+q.enqueue(3)
+q.enqueue(5)
+assert q.dequeue() == 3
+assert q.dequeue() == 5
+
+q.enqueue(7)
+q.enqueue(8)
+
+assert q.dequeue() == 7
+assert q.dequeue() == 8
